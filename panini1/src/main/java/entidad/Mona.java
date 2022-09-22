93df62522fd6d2456fc5809b4021d083abaaeda4
AlejoSuaterna <pablo.gonzalez01@correo.usa.edu.co>
2022-09-22 13:19:47 -0500

    back panini

---
+++ panini1/src/main/java/entidad/Mona.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidad;
+
+/**
+ *
+ * @author a
+ */
+public class Mona {
+    int id;
+    String nombre;
+    String equipo;
+    String img;
+
+    public Mona(int id, String nombre, String equipo, String img) {
+        this.id = id;
+        this.nombre = nombre;
+        this.equipo = equipo;
+        this.img = img;
+    }
+      
+}
---
+++ panini1/src/main/java/entidad/Usuario.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidad;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author a
+ */
+public class Usuario {
+    String user;
+    int id;
+    String password;
+    String nombre;
+    String apellido;
+    ArrayList<Mona> monas;
+
+    public Usuario(String user, int id, String password, String nombre, String apellido, ArrayList<Mona> monas) {
+        this.user = user;
+        this.id = id;
+        this.password = password;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.monas = monas;
+    }
+    
+}
